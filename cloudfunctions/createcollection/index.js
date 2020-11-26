6a7352e108f93064c717d0b868297a2f53f5bdb3
暴风磁感线 <70740571+magnetic233@users.noreply.github.com>
2020-11-26 10:58:22 +0800

    云函数昨天没传 差点被洗了
    只要进入homepage界面就会根据usertable里的用户数据判定是否存在你的收藏表格 如果不则创建表

---
+++ cloudfunctions/createcollection/index.js
@@ -0,0 +1,39 @@
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var openid =wxContext.OPENID
+  //先创建数据库 若失败则无事发生
+  //此时更新用户表
+  var usertableinfo=await db.collection('usertable').where({
+    useropenid: openid
+  }).get()
+console.log(usertableinfo.data)
+
+  if(usertableinfo.data ==  '')
+  {
+    console.log('usertableinfo.data == null')
+    console.log('the user is not on the usertable')
+    db.createCollection(openid)
+    db.collection('usertable').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        useropenid:openid,
+        userName:event.nickName,
+        userimg:event.avatarUrl
+      }
+    })
+    console.log('add info success')
+   
+  }else{
+    console.log('usertableinfo.data != null')
+    console.log('the user is on the usertable')
+  }
+
+
+
+
+
+}
